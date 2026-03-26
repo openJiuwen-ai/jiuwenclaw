a3571070d7fd25670ac7f014cf1b8fcaffb99ce0
cycong_1 <changyucong1@huawei.com>
2026-03-26 10:40:58 +0800

    refactor(jiuwenclaw): 增加tool rail添加工具提示词

--- jiuwenclaw/agentserver/rails/__init__.py
+++ jiuwenclaw/agentserver/rails/__init__.py
@@ -3,7 +3,9 @@
 """JiuWenClaw Rails for DeepAgent integration."""
 
 from jiuwenclaw.agentserver.rails.stream_event_rail import JiuClawStreamEventRail
+from jiuwenclaw.agentserver.rails.tool_prompt_rail import ToolPromptRail
 
 __all__ = [
     "JiuClawStreamEventRail",
+    "ToolPromptRail",
 ]
