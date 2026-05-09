1dfbd1924fadae72f3cc2ae40e31da56df30db04
chen-hui-zhe-xi <chenhui280@h-partners.com>
2026-05-09 09:57:35 +0800

    update config.yaml
    Co-authored-by: chen hui<chenhui280@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1312 merge update-conf into enterprise_dev
    
    update config.yaml
    
    Created-by: chen-hui-zhe-xi
    Commit-by: chen hui
    Merged-by: cyz95
    Description: <!--  Thanks for sending a pull request!  Here are some tips for you:
    
    1) If this is your first time, please read our contributor guidelines: https://gitcode.com/openJiuwen/community/blob/master/CONTRIBUTING.md
    
    2) If you want to contribute your code but don't know who will review and merge, please add label `openjiuwen-assistant` to the pull request, we will find and do it as soon as possible.
    -->
    
    **What type of PR is this?**
    <!-- 
    选择下面一种标签替换下方 `/kind <label>`，可选标签类型有：
    - /kind bug 
    - /kind task
    - /kind feature
    - /kind refactor
    - /kind clean_code
    如PR描述不符合规范，修改PR描述后需要/check-pr重新检查PR规范。
    -->
    /kind feature
    
    
    **Self-checklist**:（**请自检，在[ ]内打上x，我们将检视你的完成情况，否则会导致pr无法合入**）
    
    + - [x] **设计**：PR对应的方案是否已经经过Maintainer评审，方案检视意见是否均已答复并完成方案修改
    + - [x] **测试**：PR中的代码是否已有UT/ST测试用例进行充分的覆盖，新增测试用例是否随本PR一并上库或已经上库
    + - [x] **验证**：PR描述信息中是否已包含对该PR对应的Feature、Refactor、Bugfix的预期目标达成情况的详细验证结果描述
    + - [x] **接口**：是否涉及对外接口变更，相应变更已得到接口评审组织的通过，API对应的注释信息已经刷新正确
    + - [x] **文档**：是否涉及官网文档修改，如果涉及请及时提交资料到Doc仓
    
    <!-- **Special notes for your reviewers**: -->
    <!-- + - [ ] 是否导致无法前向兼容 -->
    <!-- + - [ ] 是否涉及依赖的三方库变更 -->
    
    See merge request: openJiuwen/jiuwenclaw!1312

--- deploy/gateway_handler.sh
+++ deploy/gateway_handler.sh
@@ -4,18 +4,20 @@ set -euo >/dev/null 2>&1
 
 gen_gateway_config_file() {
     local client_type="${DEPLOY_VARS["AGENT_RUNTIME"]}"
+    local field_name="feishu"
     GATEWAY_CONFIG_TEMPLATE_FILE="${SCRIPT_DIR}/conf/gateway-config-${client_type}.template.yaml"
 
     info "AGENT_RUNTIME: ${client_type}"
     if [ "${client_type}" == "yuanrong" ]; then
         collect_oyr_info
+        field_name="feishu_enterprise"
     fi
     info "GATEWAY_CONFIG_TEMPLATE_FILE: ${GATEWAY_CONFIG_TEMPLATE_FILE}"
 
     render_config_template ${GATEWAY_CONFIG_TEMPLATE_FILE} ${GATEWAY_CONFIG_FILE} "DEPLOY_VARS"
 
     # Clear configuration
-    yq eval '.channels.feishu_enterprise = {}' -i "${GATEWAY_CONFIG_FILE}"
+    yq eval ".channels.${field_name} = {}" -i "${GATEWAY_CONFIG_FILE}"
 
     echo "${DEPLOY_VARS["FEISHU_BOTS"]}" | while read -r line; do
         # Skip empty lines
@@ -25,14 +27,14 @@ gen_gateway_config_file() {
         IFS=':' read -r bot_name app_id app_secret <<< "${line}"
 
         # info "Adding bot: ${bot_name}"
-        yq eval ".channels.feishu_enterprise.${bot_name}.app_id = \"${app_id}\"" -i "${GATEWAY_CONFIG_FILE}"
-        yq eval ".channels.feishu_enterprise.${bot_name}.app_secret = \"${app_secret}\"" -i "${GATEWAY_CONFIG_FILE}"
-        yq eval ".channels.feishu_enterprise.${bot_name}.encrypt_key = \"\"" -i "${GATEWAY_CONFIG_FILE}"
-        yq eval ".channels.feishu_enterprise.${bot_name}.verification_token = \"\"" -i "${GATEWAY_CONFIG_FILE}"
-        yq eval ".channels.feishu_enterprise.${bot_name}.allow_from = []" -i "${GATEWAY_CONFIG_FILE}"
-        yq eval ".channels.feishu_enterprise.${bot_name}.enable_streaming = true" -i "${GATEWAY_CONFIG_FILE}"
-        yq eval ".channels.feishu_enterprise.${bot_name}.chat_id = \"\"" -i "${GATEWAY_CONFIG_FILE}"
-        yq eval ".channels.feishu_enterprise.${bot_name}.enabled = true" -i "${GATEWAY_CONFIG_FILE}"
+        yq eval ".channels.${field_name}.${bot_name}.app_id = \"${app_id}\"" -i "${GATEWAY_CONFIG_FILE}"
+        yq eval ".channels.${field_name}.${bot_name}.app_secret = \"${app_secret}\"" -i "${GATEWAY_CONFIG_FILE}"
+        yq eval ".channels.${field_name}.${bot_name}.encrypt_key = \"\"" -i "${GATEWAY_CONFIG_FILE}"
+        yq eval ".channels.${field_name}.${bot_name}.verification_token = \"\"" -i "${GATEWAY_CONFIG_FILE}"
+        yq eval ".channels.${field_name}.${bot_name}.allow_from = []" -i "${GATEWAY_CONFIG_FILE}"
+        yq eval ".channels.${field_name}.${bot_name}.enable_streaming = true" -i "${GATEWAY_CONFIG_FILE}"
+        yq eval ".channels.${field_name}.${bot_name}.chat_id = \"\"" -i "${GATEWAY_CONFIG_FILE}"
+        yq eval ".channels.${field_name}.${bot_name}.enabled = true" -i "${GATEWAY_CONFIG_FILE}"
     done
 
     success "ConfigMap file generation completed: ${GATEWAY_CONFIG_FILE}"
@@ -116,6 +118,7 @@ deploy_gateway() {
 
     # create configMap from config.yaml
     gen_gateway_config_file
+
     info "Executing: kubectl create configmap -n ${namespace} ${conf_name} --from-file=config.yaml=${GATEWAY_CONFIG_FILE} --dry-run=client -o yaml | kubectl apply -f -"
     kubectl create configmap -n ${namespace} ${conf_name} --from-file=config.yaml=${GATEWAY_CONFIG_FILE} --dry-run=client -o yaml | kubectl apply -f -
 
---
+++ deploy/update_conf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+SRC_FILE=${PROJECT_DIR}/jiuwenclaw/resources/config.yaml
+DEST_FILE=${PROJECT_DIR}/deploy/conf/gateway-config-jiuwen.template.yaml
+
+rm -f "${DEST_FILE}"
+cp -f "${SRC_FILE}" "${DEST_FILE}"
+
+yq eval '.gateway.agent_client.type = "jiuwen"' -i "${DEST_FILE}"
+yq eval '.gateway.session_map_scope = "<<GATEWAY_SESSION_MAP_SCOPE>>"' -i "${DEST_FILE}"
+yq eval '.channels.feishu = {}' -i "${DEST_FILE}"
+yq eval '.extensions.extension_dirs = "/app/jiuwenclaw/docker/extensions"' -i "${DEST_FILE}"
+yq eval '.extensions.agent_client_rest.host = "${AGENT_CLIENT_REST_HOST:-0.0.0.0}"' -i "${DEST_FILE}"
+
