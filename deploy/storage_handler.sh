8d395d6fa8480d85a51663f336146c3df5ebd8e8
chen-hui-zhe-xi <chenhui280@h-partners.com>
2026-05-19 16:35:41 +0800

    export manager service
    Co-authored-by: chen hui<chenhui280@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1630 merge export-manager-service into dev/enterprise_kub
    
    export manager service
    
    Created-by: chen-hui-zhe-xi
    Commit-by: chen hui
    Merged-by: cyz95
    Description: <!--  Thanks for sending a pull request!  Here are some tips for you:
    
    1) If this is your first time, please read our contributor guidelines: https://gitcode.com/openJiuwen/community/blob/master/CONTRIBUTING.md
    
    2) If you want to contribute your code but don't know who will review and merge, please add label `openjiuwen-assistant` to the pull request, we will find and do it as soon as possible.
    -->
    
    **What type of PR is this?**
    <!-- 
    选择下面一种标签替换下方 `/kind <label>`，可选标签类型有：
    - /kind bug 
    - /kind task
    - /kind feature
    - /kind refactor
    - /kind clean_code
    如PR描述不符合规范，修改PR描述后需要/check-pr重新检查PR规范。
    -->
    /kind feature
    
    
    **Self-checklist**:（**请自检，在[ ]内打上x，我们将检视你的完成情况，否则会导致pr无法合入**）
    
    + - [x] **设计**：PR对应的方案是否已经经过Maintainer评审，方案检视意见是否均已答复并完成方案修改
    + - [x] **测试**：PR中的代码是否已有UT/ST测试用例进行充分的覆盖，新增测试用例是否随本PR一并上库或已经上库
    + - [x] **验证**：PR描述信息中是否已包含对该PR对应的Feature、Refactor、Bugfix的预期目标达成情况的详细验证结果描述
    + - [x] **接口**：是否涉及对外接口变更，相应变更已得到接口评审组织的通过，API对应的注释信息已经刷新正确
    + - [x] **文档**：是否涉及官网文档修改，如果涉及请及时提交资料到Doc仓
    
    <!-- **Special notes for your reviewers**: -->
    <!-- + - [ ] 是否导致无法前向兼容 -->
    <!-- + - [ ] 是否涉及依赖的三方库变更 -->
    
    See merge request: openJiuwen/jiuwenswarm!1630

--- deploy/storage_handler.sh
+++ deploy/storage_handler.sh
@@ -20,10 +20,10 @@ deploy_nfs() {
     local nfs_path=${DEPLOY_VARS["NFS_HOST_PATH"]}
     local nfs_dname=${DEPLOY_VARS["NFS_NAME"]}
 
-    render_config_template ${NFS_SERVER_TEMPLATE_FILE} ${NFS_SERVER_FILE} "DEPLOY_VARS"
+    render_config_template ${NFS_TEMPLATE_FILE} ${NFS_FILE} "DEPLOY_VARS"
     exec_cmd mkdir -p ${nfs_path}
     exec_cmd chmod -R 777 ${nfs_path}
-    exec_cmd kubectl apply -f ${NFS_SERVER_FILE}
+    exec_cmd kubectl apply -f ${NFS_FILE}
     wait_k8s_resource_ready "deployment" "${nfs_dname}"
 }
 
