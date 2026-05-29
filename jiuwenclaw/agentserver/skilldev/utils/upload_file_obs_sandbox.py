9423df4d52bb661ad35bb1c118ccfbea807a411d
yanghui79 <yanghui79@noreply.gitcode.com>
2026-05-29 16:03:21 +0800

    feat(skilldev): 上传obs
    Co-authored-by: shilishan<1026783506@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2117 merge enterprise_vibeskill_dev_0512 into enterprise_vibeskill_dev
    
    feat(skilldev): 上传obs
    
    Created-by: shilishan2222
    Commit-by: shilishan
    Merged-by: yanghui79
    Description: <!--  Thanks for sending a pull request!  Here are some tips for you:
    
    1) If this is your first time, please read our contributor guidelines: https://gitcode.com/openJiuwen/community/blob/master/CONTRIBUTING.md
    
    2) If you want to contribute your code but don't know who will review and merge, please add label `openjiuwen-assistant` to the pull request, we will find and do it as soon as possible.
    -->
    
    **What type of PR is this?**
    <!-- 
    选择下面一种标签替换下方 `/kind <label>`，可选标签类型有：
    - /kind bug 
    - /kind task
    - /kind feature
    - /kind refactor
    - /kind clean_code
    如PR描述不符合规范，修改PR描述后需要/check-pr重新检查PR规范。
    -->
    /kind feature
    
    
    **Self-checklist**:（**请自检，在[ ]内打上x，我们将检视你的完成情况，否则会导致pr无法合入**）
    
    + - [ ] **设计**：PR对应的方案是否已经经过Maintainer评审，方案检视意见是否均已答复并完成方案修改
    + - [ ] **测试**：PR中的代码是否已有UT/ST测试用例进行充分的覆盖，新增测试用例是否随本PR一并上库或已经上库
    + - [ ] **验证**：PR描述信息中是否已包含对该PR对应的Feature、Refactor、Bugfix的预期目标达成情况的详细验证结果描述
    + - [ ] **接口**：是否涉及对外接口变更，相应变更已得到接口评审组织的通过，API对应的注释信息已经刷新正确
    + - [ ] **文档**：是否涉及官网文档修改，如果涉及请及时提交资料到Doc仓
    
    <!-- **Special notes for your reviewers**: -->
    <!-- + - [ ] 是否导致无法前向兼容 -->
    <!-- + - [ ] 是否涉及依赖的三方库变更 -->
    
    
    See merge request: openJiuwen/jiuwenswarm!2117

--- jiuwenclaw/agentserver/skilldev/utils/upload_file_obs_sandbox.py
+++ jiuwenclaw/agentserver/skilldev/utils/upload_file_obs_sandbox.py
@@ -1,24 +1,208 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) Huawei Technologies Co., Ltd. 2026-2026. All rights reserved.
+import time
+import aiohttp
+import hashlib
+import os
 import uuid
 import logging
+import requests
 
+from jiuwenclaw.utils import build_default_headers
 
 logger = logging.getLogger(__name__)
+OSMS_SLB_URL = os.environ.get('SANDBOX_OSMS_SLB_URL', 'null')
+logger.info("OSMS_SLB_URL: %s", OSMS_SLB_URL)
+osms_prepare_url = OSMS_SLB_URL.rstrip('/') + '/osms/v1/file/manager/prepare'
+osms_complete_query_url = OSMS_SLB_URL.rstrip('/') + '/osms/v1/file/manager/completeAndQuery'
+
+SUCCESS_STATUS_CODE = 200
+MAX_TIMES = 3  # 执行次数
+connect_timeout = 15
+read_timeout = 300
 
 
 class UploadFileByOSMS(object):
     def __init__(self):
         self.trace_id = str(uuid.uuid4())[:16]
-        self.is_permanent_storage_public = False
 
-    async def upload_file(self, file_path: str, is_permanent_storage_public=False):
+    async def upload_file(self, file_path: str, useEdge=False):
         try:
-            self.is_permanent_storage_public = is_permanent_storage_public
-            updload_file_info = await self.upload_file_and_get_info(file_path)
-            if isinstance(updload_file_info, str):
+            updload_file_info = await self.upload_file_and_get_info(file_path, useEdge=useEdge)
+            logger.info(f"upload_file_info:{updload_file_info}")
+            if isinstance(updload_file_info, str) or not updload_file_info:
                 return None
             return updload_file_info["url"]
         except Exception as e:
-            logger.error("上传文件到osms失败")
+            logger.error(f"上传文件失败:{e}", exc_info=True)
 
-    async def upload_file_and_get_info(self, file_path: str):
-        return ""
+    async def upload_file_and_get_info(self, file_path: str, useEdge=False):
+        if not file_path:
+            logger.error("文件路径为空，无法上传")
+            return "文件路径为空"
+
+        if not os.path.exists(file_path):
+            logger.error(f"文件{file_path}不存在")
+            return "文件不存在"
+
+        if not os.path.isfile(file_path):
+            logger.error(f"{file_path}不是文件")
+            return "路径不是文件"
+
+        file_info = await self.invoking_osms_prepare(file_path)
+        logger.info(f"1.开始做上传文件准备: {file_path}")
+        if not file_info:
+            logger.error(f"{file_path} 上传OSMS失败")
+            return "{}:文件上传失败OSMS".format(os.path.basename(file_path))
+
+        logger.info(f"2.开始上传: {file_path}")
+        result = await self.read_file_as_bytes(file_path)
+        if not result["success"]:
+            return "{}:{}".format(result["file_name"], result["error"])
+        upload_file_info = await self.upload_file_to_obs(file_info["uploadInfos"][0], result["bytes"])
+
+        if not upload_file_info:
+            logger.error(f"{file_path} 上传OBS失败")
+            return "{}:上传文件到 obs 报错".format(os.path.basename(file_path))
+
+        logger.info(f"3.查看上传结果: {file_path}")
+        fileDetailInfos = await self.invoking_osms_complete_and_query(file_info, useEdge=useEdge)
+        if not fileDetailInfos:
+            logger.error(f"获取osms的cdn链接失败：{file_path}")
+        return fileDetailInfos
+
+    async def read_file_as_bytes(self, file_path):
+        # 读取任意文件的原始二进制内容
+        result = {
+            "success": False,
+            "file_name": os.path.basename(file_path),
+            "bytes": None,
+            "error": None
+        }
+        try:
+            with open(file_path, 'rb') as f:
+                byte_content = f.read()
+                result.update({
+                    "success": True,
+                    "bytes": byte_content
+                })
+        except PermissionError:
+            result["error"] = "没有读取权限"
+        except Exception as e:
+            logger.error(f"读取文件失败: {e}", exc_info=True)
+            result["error"] = f"读取失败: {str(e)}"
+        return result
+
+    async def upload_file_to_obs(self, file_info, file_bytes):
+        retry_delay = 1  # 初始重试延迟(秒)
+        retry_time = 1  # 当前块重试次数
+        while True:
+            try:
+                response = requests.put(file_info["url"], data=file_bytes, headers=file_info["headers"],
+                                        timeout=(connect_timeout, read_timeout))
+                logger.info(f"response: {response}")
+                response.raise_for_status()
+                return True
+            except Exception as e:
+                logger.error(f"upload file to obs fail:{e}", exc_info=True)
+                retry_time += 1
+                time.sleep(retry_delay * (2 ** retry_time))
+                if retry_time > MAX_TIMES:
+                    logger.error("{}上传文件到 obs 报错{}".format(file_info["url"], str(e)))
+                    return False
+
+    async def _calculate_file_sha256(self, file_path):
+        sha256 = hashlib.sha256()
+        with open(file_path, 'rb') as f:
+            while chunk := f.read(4096):
+                sha256.update(chunk)
+        return sha256.hexdigest()
+
+    def _build_headers(self):
+        default_headers = build_default_headers()
+        return {
+            "Content-Type": "application/json",
+            "x-sandbox-id": default_headers.get("x-sandbox-id", ""),
+            "x-api-key": default_headers.get("x-api-key", ""),
+            "x-hag-trace-id": default_headers.get("x-hag-trace-id", ""),
+            "x-request-from": "jiuwenclaw"  # 不能修改
+        }
+
+    async def invoking_osms_prepare(self, file_path):
+        headers = self._build_headers()
+        body = {
+            "objectType": "COMMON_PRIVATE",
+            "fileName": os.path.basename(file_path),
+            "fileSha256": await self._calculate_file_sha256(file_path),
+            "fileSize": os.path.getsize(file_path),
+            "fileOwnerInfo": {
+                "uid": "jiuwenclaw",
+                "teamId": "jiuwenclaw"
+            },
+            "useEdge": False
+        }
+
+        for times in range(MAX_TIMES):
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.post(url=osms_prepare_url, headers=headers, json=body, ssl=True) as response:
+                        logger.info(f"Invoke prepare, post to {osms_prepare_url} response: {response}")
+                        if response.status != SUCCESS_STATUS_CODE:
+                            text = await response.text()
+                            raise RuntimeError(f"Invoke prepare failed, SUCCESS_STATUS_CODE={SUCCESS_STATUS_CODE}, " +
+                                               f"but status={response.status}, body={text}")
+
+                        resp = await response.json()
+                        logger.info(f"prepare interface business resp: {resp}")
+                        if resp.get("code") != '0':
+                            raise RuntimeError(f"prepare interface business error: {resp}")
+                        if 'objectId' not in resp or 'draftId' not in resp or 'uploadInfos' not in resp:
+                            raise RuntimeError("The osms prepare interface returns an exception")
+                        if not resp["uploadInfos"]:
+                            raise RuntimeError("The osms prepare interface uploadInfos returns is empty")
+                        if 'url' not in resp["uploadInfos"][0] or 'headers' not in resp["uploadInfos"][0]:
+                            raise RuntimeError("The osms prepare interface url and headers for uploadInfos is empty")
+                        return resp
+            except Exception as e:
+                logger.error(f"{times + 1}st invoking osms prepare interface throws exception: {e}", exc_info=True)
+                if times == MAX_TIMES - 1:
+                    raise RuntimeError(
+                        f"osms prepare interface call failed after retry limit, type: {type(e).__name__}", e) from e
+        return {}
+
+    async def invoking_osms_complete_and_query(self, file_info, useEdge=False):
+        headers = self._build_headers()
+        body = {
+            "objectId": file_info["objectId"],
+            "draftId": file_info["draftId"],
+            "expireTime": 3600,
+            # completeAndQuery 调skill_scan的话，就带useEdge=true
+            "useEdge": useEdge
+        }
+        for times in range(MAX_TIMES):
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.post(url=osms_complete_query_url, headers=headers, json=body,
+                                            ssl=True) as response:
+                        logger.info(f"Invoke completeAndQuery, post to {osms_complete_query_url} response: {response}")
+                        if response.status != SUCCESS_STATUS_CODE:
+                            text = await response.text()
+                            raise RuntimeError(
+                                f"Invoke completeAndQuery failed, SUCCESS_STATUS_CODE={SUCCESS_STATUS_CODE}, " +
+                                f"but status={response.status}, body={text}")
+                        resp = await response.json()
+                        logger.info(f"completeAndQuery interface business resp: {resp}")
+                        if resp.get("code") != '0':
+                            raise RuntimeError(f"completeAndQuery interface business error: {resp}")
+                        file_detail = resp.get("fileDetailInfo")
+                        if not file_detail or 'url' not in file_detail:
+                            raise RuntimeError("The osms completeAndQuery interface fileDetailInfo.url is empty")
+                        return file_detail
+            except Exception as e:
+                logger.error(f"{times + 1}st invoking osms completeAndQuery interface throws exception：{e}",
+                             exc_info=True)
+                if times == MAX_TIMES - 1:
+                    raise RuntimeError(
+                        f"osms completeAndQuery interface call failed after retry limit, type :{type(e).__name__}",
+                        e) from e
+        return None
