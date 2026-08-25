4319dd6fa30103e5b4c9d98fbc619495783e3a07
chen hui <chenhui280@h-partners.com>
2026-08-25 11:10:58 +0800

    fix deloyment boot issue

--- deploy/enterprise/postgresql_handler.sh
+++ deploy/enterprise/postgresql_handler.sh
@@ -2,26 +2,26 @@
 set -euo >/dev/null 2>&1
 
 render_postgresql_files() {
-    local template_file="${CONFIG["POSTGRES_TEMPLATE_FILE"]}"
-    local file="${CONFIG["POSTGRES_FILE"]}"
+    local template_file="${CONFIG["POSTGRESQL_TEMPLATE_FILE"]}"
+    local file="${CONFIG["POSTGRESQL_FILE"]}"
 
-    ensure_available_port "POSTGRES_NODE_PORT"
+    ensure_available_port "POSTGRESQL_NODE_PORT"
     render_config_template "${template_file}" "${file}" "DEPLOY_VARS"
-    add_resource_if_set "POSTGRES" "${file}"
+    add_resource_if_set "POSTGRESQL" "${file}"
 }
 
 deploy_postgresql() {
-    local pg_name="${DEPLOY_VARS["POSTGRES_NAME"]}"
-    local file="${CONFIG["POSTGRES_FILE"]}"
+    local pg_name="${DEPLOY_VARS["POSTGRESQL_NAME"]}"
+    local file="${CONFIG["POSTGRESQL_FILE"]}"
     
     exec_cmd kubectl apply -f ${file}
     wait_k8s_resource_ready "statefulset" "${pg_name}"
-    success "POSTGRES_NODE_PORT: ${DEPLOY_VARS["POSTGRES_NODE_PORT"]}"
+    success "POSTGRESQL_NODE_PORT: ${DEPLOY_VARS["POSTGRESQL_NODE_PORT"]}"
 }
 
 uninstall_postgresql() {
-    local pg_name="${DEPLOY_VARS["POSTGRES_NAME"]}"
-    local file="${CONFIG["POSTGRES_FILE"]}"
+    local pg_name="${DEPLOY_VARS["POSTGRESQL_NAME"]}"
+    local file="${CONFIG["POSTGRESQL_FILE"]}"
 
     exec_cmd kubectl delete -f ${file} --ignore-not-found=true
     wait_pod_terminated "${pg_name}"
