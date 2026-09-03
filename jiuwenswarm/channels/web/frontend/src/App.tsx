1287f8d3b278fe9ac0d2097f7a7d22856ff1e3a7
l_x_d <linxiangda@huawei.com>
2026-09-03 19:31:26 +0800

    fix(rsi): render experiment page once

--- jiuwenswarm/channels/web/frontend/src/App.tsx
+++ jiuwenswarm/channels/web/frontend/src/App.tsx
@@ -3005,11 +3005,6 @@ function AppContent({
             />
           </div>
         )}
-        {activeNav === 'experiments' && (
-          <div className="app-section">
-            <RsiPage />
-          </div>
-        )}
         {activeNav === 'teams' && (
           <div className="app-section">
             <TeamPanel onSessionsDeleted={handleTeamSessionsDeleted} />
