openJiuwen-ai/jiuwenswarm

feat(agentos): reject Web/TUI WS upgrade unless IAM auth succeeds
Verify the token on the HTTP handshake and return 401 instead of
accepting the WebSocket first. Do not verify again in on_connect.

Co-authored-by: Cursor <cursoragent@cursor.com>

tests/unit_tests/channel/test_web_channel_dual_protocol.py
@@ -17,8 +17,10 @@ from unittest.mock import AsyncMock, MagicMock
 import pytest
 
 from jiuwenswarm.gateway.channel_manager.base import RobotMessageRouter
+from jiuwenswarm.extensions.agentos.auth.credential_authenticator import AuthResult
 from jiuwenswarm.gateway.channel_manager.web.web_channel_app import (
     _reject_disallowed_origin,
+    _serve_channel_websocket,
     build_web_channel_app,
     normalize_web_ws_path,
 )
@@ -186,3 +188,24 @@ def test_webchannel_config_dual_protocol_default_true() -> None:
 
 def test_webchannel_config_legacy_flag() -> None:
     assert WebChannelConfig(dual_protocol=False).dual_protocol is False
+
+
+@pytest.mark.asyncio
+async def test_serve_ws_does_not_accept_when_unauthorized() -> None:
+    channel = _make_channel()
+
+    async def deny(**_kwargs: Any) -> AuthResult:
+        return AuthResult(success=False, error="Token 无效或已过期")
+
+    channel.on_handshake_auth(deny)
+    websocket = MagicMock()
+    websocket.headers = {}
+    websocket.url.path = "/ws"
+    websocket.url.query = "token=fake"
+    websocket.client = MagicMock(host="127.0.0.1", port=9)
+    websocket.send_denial_response = AsyncMock()
+    websocket.accept = AsyncMock()
+    await _serve_channel_websocket(channel, websocket)
+    websocket.accept.assert_not_called()
+    websocket.send_denial_response.assert_awaited_once()
+    assert websocket.send_denial_response.await_args.args[0].status_code == 401

tests/unit_tests/test_agentos_ws_handshake_auth.py
@@ -1,0 +1,44 @@
+# Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+
+from __future__ import annotations
+
+from typing import Any
+
+import pytest
+
+from jiuwenswarm.extensions.agentos.auth.credential_authenticator import AuthResult
+from jiuwenswarm.gateway.app_gateway import GatewayServer, GatewayServerConfig, RouteConfig
+from jiuwenswarm.gateway.channel_manager.base import RobotMessageRouter
+from jiuwenswarm.gateway.channel_manager.web.web_connect import WebChannel, WebChannelConfig
+
+
+async def _deny(**_kwargs: Any) -> AuthResult:
+    return AuthResult(success=False, error="缺少 token")
+
+
+@pytest.mark.asyncio
+async def test_webchannel_process_request_rejects_unauthorized() -> None:
+    channel = WebChannel(WebChannelConfig(enabled=True), RobotMessageRouter())
+    channel.on_handshake_auth(_deny)
+    status, _headers, body = await channel._process_request("/ws", {})
+    assert int(status) == 401
+    assert body == b"Unauthorized\n"
+
+
+@pytest.mark.asyncio
+async def test_gateway_server_process_request_rejects_tui_not_acp() -> None:
+    tui = WebChannel(WebChannelConfig(enabled=True), RobotMessageRouter())
+    tui.on_handshake_auth(_deny)
+    server = GatewayServer(
+        GatewayServerConfig(
+            enabled=True,
+            routes={
+                "/tui": RouteConfig(path="/tui", channel_id="tui", ws_channel=tui),
+                "/acp": RouteConfig(path="/acp", channel_id="acp"),
+            },
+        ),
+        RobotMessageRouter(),
+    )
+    status, _headers, body = await server._process_request("/tui", {})
+    assert int(status) == 401
+    assert await server._process_request("/acp", {}) is None

openJiuwen-ai/jiuwenclaw

tests/unit_tests/channel/test_web_channel_dual_protocol.py
@@ -17,8 +17,10 @@ from unittest.mock import AsyncMock, MagicMock
 import pytest
 
 from jiuwenswarm.gateway.channel_manager.base import RobotMessageRouter
+from jiuwenswarm.extensions.agentos.auth.credential_authenticator import AuthResult
 from jiuwenswarm.gateway.channel_manager.web.web_channel_app import (
     _reject_disallowed_origin,
+    _serve_channel_websocket,
     build_web_channel_app,
     normalize_web_ws_path,
 )
@@ -186,3 +188,24 @@ def test_webchannel_config_dual_protocol_default_true() -> None:
 
 def test_webchannel_config_legacy_flag() -> None:
     assert WebChannelConfig(dual_protocol=False).dual_protocol is False
+
+
+@pytest.mark.asyncio
+async def test_serve_ws_does_not_accept_when_unauthorized() -> None:
+    channel = _make_channel()
+
+    async def deny(**_kwargs: Any) -> AuthResult:
+        return AuthResult(success=False, error="Token 无效或已过期")
+
+    channel.set_handshake_auth(deny)
+    websocket = MagicMock()
+    websocket.headers = {}
+    websocket.url.path = "/ws"
+    websocket.url.query = "token=fake"
+    websocket.client = MagicMock(host="127.0.0.1", port=9)
+    websocket.send_denial_response = AsyncMock()
+    websocket.accept = AsyncMock()
+    await _serve_channel_websocket(channel, websocket)
+    websocket.accept.assert_not_called()
+    websocket.send_denial_response.assert_awaited_once()
+    assert websocket.send_denial_response.await_args.args[0].status_code == 401

tests/unit_tests/test_agentos_ws_handshake_auth.py
@@ -1,0 +1,44 @@
+# Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+
+from __future__ import annotations
+
+from typing import Any
+
+import pytest
+
+from jiuwenswarm.extensions.agentos.auth.credential_authenticator import AuthResult
+from jiuwenswarm.gateway.app_gateway import GatewayServer, GatewayServerConfig, RouteConfig
+from jiuwenswarm.gateway.channel_manager.base import RobotMessageRouter
+from jiuwenswarm.gateway.channel_manager.web.web_connect import WebChannel, WebChannelConfig
+
+
+async def _deny(**_kwargs: Any) -> AuthResult:
+    return AuthResult(success=False, error="缺少 token")
+
+
+@pytest.mark.asyncio
+async def test_webchannel_process_request_rejects_unauthorized() -> None:
+    channel = WebChannel(WebChannelConfig(enabled=True), RobotMessageRouter())
+    channel.set_handshake_auth(_deny)
+    status, _headers, body = await channel._process_request("/ws", {})
+    assert int(status) == 401
+    assert body == b"Unauthorized\n"
+
+
+@pytest.mark.asyncio
+async def test_gateway_server_process_request_rejects_tui_not_acp() -> None:
+    tui = WebChannel(WebChannelConfig(enabled=True), RobotMessageRouter())
+    tui.set_handshake_auth(_deny)
+    server = GatewayServer(
+        GatewayServerConfig(
+            enabled=True,
+            routes={
+                "/tui": RouteConfig(path="/tui", channel_id="tui", ws_channel=tui),
+                "/acp": RouteConfig(path="/acp", channel_id="acp"),
+            },
+        ),
+        RobotMessageRouter(),
+    )
+    status, _headers, body = await server._process_request("/tui", {})
+    assert int(status) == 401
+    assert await server._process_request("/acp", {}) is None